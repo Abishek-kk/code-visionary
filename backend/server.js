bb2069b9b5c6be0bde80ecb2cba143b78274be42
Abishek-kk <abishekkumar548709@gmail.com>
2026-06-03 10:46:41 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ if (FRONTEND_URL) {
   });
 }
 
-app.use(cors({
+const corsOptions = {
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
 
@@ -40,7 +40,10 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
-}));
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(express.json({ limit: '50kb' }));
 
